1738a462f346af8eba81bf84eaf1a8a8af333bcd
axiaoxin <254606826@qq.com>
2015-03-21 18:31:57 +0800

    add docstr

--- mbti/views.py
+++ mbti/views.py
@@ -6,9 +6,11 @@ MBTI_BP = Blueprint('mbti', __name__)
 
 @MBTI_BP.route('/')
 def home():
+    '''首页'''
     return render_template('mbti/home.html')
 
 
-@MBTI_BP.route('/test')
+@MBTI_BP.route('/test', methods=('GET, POST'))
 def test():
+    '''测试'''
     return render_template('mbti/test.html')
