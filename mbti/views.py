070035ed35ddf2d54b4e6a94fcf462b172e9ab8e
axiaoxin <254606826@qq.com>
2015-03-22 05:50:29 +0800

    init

--- mbti/views.py
+++ mbti/views.py
@@ -4,13 +4,21 @@ from flask import Blueprint
 
 MBTI_BP = Blueprint('mbti', __name__)
 
+
 @MBTI_BP.route('/')
+@MBTI_BP.route('/welcome/')
+def welcome():
+    '''欢迎页面，纯属装逼'''
+    return render_template('mbti/welcome.html')
+
+
+@MBTI_BP.route('/home')
 def home():
-    '''首页'''
+    '''这才是真正的首页'''
     return render_template('mbti/home.html')
 
 
-@MBTI_BP.route('/test', methods=('GET, POST'))
+@MBTI_BP.route('/test/', methods=('GET, POST'))
 def test():
-    '''测试'''
+    '''测试页面视图'''
     return render_template('mbti/test.html')
