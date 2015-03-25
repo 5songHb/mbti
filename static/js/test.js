aa003382d138450fbcc7b4989971a5f981423721
axiaoxin <254606826@qq.com>
2015-03-26 00:28:24 +0800

    typo fixed

--- static/js/test.js
+++ static/js/test.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             form.remove();
             next_form.css("display", "block");
         }, 520);
-        if (answers.length == 3){
+        if (answers.length == 93){
             $.post("/test/",
                    {"answers": JSON.stringify(answers)},
                    function(data){
