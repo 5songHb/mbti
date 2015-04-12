6331e5ff91545afb7fcbb144d7d5b9a55ee7fcc9
axiaoxin <254606826@qq.com>
2015-04-12 10:42:56 +0800

    pep8

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
 from __future__ import unicode_literals
-from flask import Flask
-from flask import render_template
+
+from flask import Flask, render_template
+
 from mbti import MBTI_BP
 
 SERVER = Flask(__name__)
 SERVER.config.from_pyfile('settings.py')
 
+
 @SERVER.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
 
 @SERVER.errorhandler(500)
 def server_error(error):
